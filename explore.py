21b09b507bab4e6ecc32218ed6f30b3b2cf3a0b9
Claire Wu <claire.xy@gmail.com>
2020-02-23 11:21:03 +0000

    Add map easter eggs

--- explore.py
+++ explore.py
@@ -15,13 +15,24 @@ def run():
     elif q == "Plants":
         user_input = st.text_input("enter plant")
 
+    start_loc=[51.5262, -0.1607]
+    zoo_loc = [51.5353,-0.1534]
+
     if user_input == "":
         df = pd.DataFrame(
-            np.array([[51.52903, -0.155  ]]), columns=['lat', 'lon'])
-        st.map(df, zoom = 14)
-    else:
-        n = randint(10,100)
-
+            np.array([start_loc]), columns=['lat', 'lon'])
+        st.map(df, zoom = 13)
+    elif user_input == 'lion' or user_input == 'tiger' or user_input == 'giraffe':
         df = pd.DataFrame(
-            np.random.randn(n, 2) / [300, 300] + [51.52903, -0.155], columns=['lat', 'lon'])
+            np.array([zoo_loc]), columns=['lat', 'lon'])
+        st.map(df, zoom = 13) 
+    elif user_input == 'pigeon' or user_input == 'rat' or user_input == 'grass':
+        n = randint(100,200)
+        df = pd.DataFrame(
+            np.random.randn(n, 2) / [200, 200] + start_loc, columns=['lat', 'lon'])
+        st.map(df) 
+    else:
+        n = randint(10,70)
+        df = pd.DataFrame(
+            np.random.randn(n, 2) / [200, 200] + start_loc, columns=['lat', 'lon'])
         st.map(df)
