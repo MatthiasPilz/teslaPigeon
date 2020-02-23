1028aff2756b45760ff5b463a79d299e6f83c96a
MatthiasPilz <Matthias.Pilz@kingston.ac.uk>
2020-02-23 11:23:53 +0000

    map update (but claire's version is better...)

--- explore.py
+++ explore.py
@@ -26,6 +26,8 @@ def run():
         user_input = ph2.text_input("enter plant")
 
     if user_input == "":
+        df = pd.DataFrame(np.array([[51.52903, -0.155]]), columns=['lat', 'lon'])
+        ph3.map(df, zoom = 14)
         ph3.map()
     elif user_input == "Regents Park":
         df = pd.DataFrame(
@@ -37,24 +39,5 @@ def run():
         ph3.map(df, zoom = 14)
     elif user_input == "test":
         df = pd.DataFrame(
-            np.random.randn(40, 2) / [250, 250] + [51.52903, -0.155])
-        ph3.pydeck_chart(pdk.Deck(
-        map_style = 'mapbox://styles/mapbox/light-v9',
-        initial_view_state = pdk.ViewState(
-        latitude = 37.76,
-        longitude = -122.4,
-        zoom = 11,
-        pitch = 50, ),
-        layers = [  pdk.Layer(     'HexagonLayer', data = df,
-        get_position = '[lon, lat]',
-        radius = 200,
-        elevation_scale = 4,
-        elevation_range = [0, 1000],
-        pickable = True,
-        extruded = True,  ),
-        pdk.Layer(
-        'ScatterplotLayer',
-        data = df,
-        get_position = '[lon, lat]',
-        get_color = '[200, 30, 0, 160]',
-        get_radius = 200,    ),], ))
+            np.random.randn(20, 2) / [300, 300] + [51.52903, -0.155], columns=['lat', 'lon'])
+        ph3.map(df, zoom = 14)
