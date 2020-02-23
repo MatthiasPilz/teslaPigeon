f042a57ee277b096bdebeeacb2f757e753817166
Claire Wu <claire.xy@gmail.com>
2020-02-23 11:02:51 +0000

    hardcoded fix

--- explore.py
+++ explore.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+from random import randint
 
 def run():
     q = st.selectbox("What are you looking for?", ("Locations", "Animals", "Plants"))
@@ -14,36 +15,19 @@ def run():
     elif q == "Plants":
         user_input = st.text_input("enter plant")
 
+
     if user_input == "":
-        st.map()
+        df = pd.DataFrame(
+            np.random.randn(1, 2) / [250, 250] + [51.52903, -0.155], columns=['lat', 'lon'])
+        st.map(df, zoom = 14)
     elif user_input == "Regents Park":
+        n = randint(10,100)
         df = pd.DataFrame(
-            np.random.randn(40, 2) / [250, 250] + [51.52903, -0.155], columns=['lat', 'lon'])
+            np.random.randn(n, 2) / [250, 250] + [51.52903, -0.155], columns=['lat', 'lon'])
         st.map(df, zoom = 14)
-    elif user_input == "Magpie":
+    else:
+        n = randint(10,100)
+
         df = pd.DataFrame(
-            np.random.randn(20, 2) / [300, 300] + [51.52903, -0.155], columns=['lat', 'lon'])
+            np.random.randn(n, 2) / [300, 300] + [51.52903, -0.155], columns=['lat', 'lon'])
         st.map(df, zoom = 14)
-    elif user_input == "test":
-        df = pd.DataFrame(
-            np.random.randn(40, 2) / [250, 250] + [51.52903, -0.155])
-        st.pydeck_chart(pdk.Deck(
-        map_style = 'mapbox://styles/mapbox/light-v9',
-        initial_view_state = pdk.ViewState(
-        latitude = 37.76,
-        longitude = -122.4,
-        zoom = 11,
-        pitch = 50, ),
-        layers = [  pdk.Layer(     'HexagonLayer', data = df,
-        get_position = '[lon, lat]',
-        radius = 200,
-        elevation_scale = 4,
-        elevation_range = [0, 1000],
-        pickable = True,
-        extruded = True,  ),
-        pdk.Layer(
-        'ScatterplotLayer',
-        data = df,
-        get_position = '[lon, lat]',
-        get_color = '[200, 30, 0, 160]',
-        get_radius = 200,    ),], ))
